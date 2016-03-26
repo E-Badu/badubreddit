db5b2aca530b5471cd38826afbd4d65dd03bb91f
Erica Badu <erica.a.badu@gmail.com>
2016-03-25 22:56:18 -0500

    updated home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,28 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- the following may give a token mismatch error, fix was made by
+    moving this, without script tags to app.js file -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+   
+    <title>BaduBreddit</title>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <!-- Fonts -->
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+
+    <!-- Styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+</head>
+<body>
+    <div id="content"></div>
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+</body>
+</html>
