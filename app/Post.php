23b42e3ef054858357ee18bb8d48f564be9a9430
Erica Badu <erica.a.badu@gmail.com>
2016-03-25 21:47:04 -0500

    update to post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+	* Get the user that owns the post
+	*/
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    /**
+	* Get the subbreddit that owns the psot
+	*/
+
+    public function post()
+    {
+    	return $this->belongsTo('App\Subbreddit');
+    }
+
+
+	/**
+	* Get the comments owned by the post
+	*/
+    public function comments()
+    {
+    	return $his->hasMany('App\Comment');
+    }
 }
