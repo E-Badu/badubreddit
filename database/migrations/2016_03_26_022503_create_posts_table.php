06e81455d8b5fffeefcb38cabb5e9f7b66219b09
Erica Badu <erica.a.badu@gmail.com>
2016-03-25 21:58:43 -0500

    update to posts table

--- database/migrations/2016_03_26_022503_create_posts_table.php
+++ database/migrations/2016_03_26_022503_create_posts_table.php
@@ -14,6 +14,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('url')->nullable();
+            $table->string('title');
+            $table->text('content');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('subbreddit_id')->unsigned()->index();
             $table->timestamps();
         });
     }
