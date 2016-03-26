f7e54d1dc2276e245a5a2e8bbd7b5786161ee5a6
Erica Badu <erica.a.badu@gmail.com>
2016-03-25 22:56:25 -0500

    updated app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <!-- the following may give a token mismatch error, fix was made by
+    moving this, without script tags to app.js file -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+   
     <title>Laravel</title>
 
     <!-- Fonts -->
