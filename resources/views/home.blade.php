57d2a8762e9cac446e2dbbe3226540b9ebc7720e
Erica Badu <erica.a.badu@gmail.com>
2016-03-27 00:50:33 -0500

    home blade update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,99 @@
     <div id="content"></div>
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js"></script>
+    
+    <script> // All here should be in app.js, but errors keep happening.
+    
+    var PostModel = Backbone.Model.extend({
+        urlRoot: '/api/posts/',
+        idAttribute: 'id',
+    });
+
+    var post = new PostModel();
+
+    var SubbredditModel = Backbone.Model.extend({
+        urlRoot: '/api/subbreddits/',
+        idAttribute: 'id'
+    });
+
+    var CommentModel = Backbone.Model.extend({
+        urlRoot: '/api/comments/',
+        idAttribute: 'id'
+    });
+
+    var PostsCollection = Backbone.Collection.extend({
+        url: '/api/posts/',
+        model: PostModel
+    });
+
+    var SubbredditsCollection = Backbone.Collection.extend({
+        url: '/api/subbreddits/',
+        model: SubbredditModel
+    });
+
+    var CommentsCollection = Backbone.Collection.extend({
+        url: '/api/comments/',
+        model: CommentModel
+    });
+
+    var HomeView = Backbone.View.extend({
+        el:'\
+            <div class="container">\
+                <div class="row">\
+                    <div class="three columns"></div>\
+                    <div class="six columns">\
+                        <div class="row">\
+                            <div class="twelve columns"></div>\
+                        </div>\
+                        <div class="row">\
+                            <div class="twelve columns"></div>\
+                        </div>\
+                    </div>\
+                    <div class="three columns" id="all-subbreddits"></div>\
+                </div>\
+            </div>\
+        ',
+
+        render: function() {
+            var subbreddits = new SubbredditsCollection();
+            //this is something you would test in the console  subbreddits.fetch();
+            var subbredditsListView = new SubbredditsListView({ 
+                collection: subbreddits
+            });
+            this.$el.find('#all-subbreddits').html(subbredditsListView.render().el);
+
+            return this;
+        }
+    });
+
+    var SubbredditsListView = Backbone.View.extend({
+        el: '<ul></ul>',
+
+        template: _.template('\
+            <% subbreddits.each(function(subbreddit) { %>\
+                <li><a href="#"><%= subbreddit.get("name") %></a></li>\
+            <% }) %>\
+        '),
+
+        initialize: function() {
+            this.listenTo(this.collection, 'update', this.render);
+        },
+
+        render: function() {
+            this.$el.html(this.template({ subbreddits: this.collection }));
+            return this;
+        }
+    })
+
+
+    var homeView = new HomeView();
+    $('#content').html(homeView.render().el);
+
+    
+
+    </script>
+
 </body>
 </html>
