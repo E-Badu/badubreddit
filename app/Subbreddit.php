e454785284f3bab85d27918126feacbb169f1134
Erica Badu <erica.a.badu@gmail.com>
2016-03-29 20:30:16 -0500

    updated server file with relationships

--- app/Subbreddit.php
+++ app/Subbreddit.php
@@ -6,30 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subbreddit extends Model
 {
-       /**
-	* Get the user that owns the subbreddit
-	*/
+     /**
+     * Get the user that owns the subbreddit.
+     */
     public function user()
     {
-    	return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User');
+    }
+
+     /**
+     * Get the posts of the subbreddit.
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
     }
 
     /**
-	* Get the posts of the subbreddit
-	*/
-
-    public function posts()
-    {
-    	return $this->hasMany('App\Post');
-    }
-
-
-	/**
-	* Get the subscribed users of the subbreddit
-	*/
+     * Get the subscribed users of the subbreddit.
+     */
     public function subscribedUsers()
     {
-    	return $his->belongsToMany('App\User');
+        return $this->belongsToMany('App\User')->withTimestamps();
     }
-}
 }
