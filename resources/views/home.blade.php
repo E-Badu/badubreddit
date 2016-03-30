4a3b52b5e107fc3904700f911f3eedc011789b4e
Erica Badu <erica.a.badu@gmail.com>
2016-03-29 20:31:20 -0500

    style and script for home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,26 @@
 
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+    <style>
+        body {
+            font-size: 10px;
+        }
+
+        .container {
+            width: 100%;
+        }
+
+        #all-subbreddits {
+            height: 600px;
+            overflow: scroll;
+        }
+
+        #posts {
+            height: 300px;
+            overflow: scroll;
+        }
+    </style>
 
 </head>
 <body>
@@ -24,15 +42,26 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js"></script>
-    
+    <!-- <script src="{{ asset('js/app.js') }}"></script>-->
     <script> // All here should be in app.js, but errors keep happening.
     
+$.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
     var PostModel = Backbone.Model.extend({
         urlRoot: '/api/posts/',
         idAttribute: 'id',
-    });
 
-    var post = new PostModel();
+        parse: function(response) {
+            if (response.subbreddit) {
+                response.subbreddit = new SubbredditModel(response.subbreddit);
+            }
+            return response;
+        }
+    });
 
     var SubbredditModel = Backbone.Model.extend({
         urlRoot: '/api/subbreddits/',
@@ -66,7 +95,7 @@
                     <div class="three columns"></div>\
                     <div class="six columns">\
                         <div class="row">\
-                            <div class="twelve columns"></div>\
+                            <div class="twelve columns" id="posts"></div>\
                         </div>\
                         <div class="row">\
                             <div class="twelve columns"></div>\
@@ -77,13 +106,27 @@
             </div>\
         ',
 
-        render: function() {
+        insertSubbreddits: function() {
             var subbreddits = new SubbredditsCollection();
-            //this is something you would test in the console  subbreddits.fetch();
+            subbreddits.fetch();
             var subbredditsListView = new SubbredditsListView({ 
                 collection: subbreddits
             });
             this.$el.find('#all-subbreddits').html(subbredditsListView.render().el);
+        },
+
+        insertPosts: function() {
+            var posts = new PostsCollection();
+            posts.fetch();
+            var postsListView = new PostsListView({ 
+                collection: posts
+            });
+            this.$el.find('#posts').html(postsListView.render().el);
+        },
+
+        render: function() {
+            this.insertSubbreddits();
+            this.insertPosts();
 
             return this;
         }
@@ -106,13 +149,34 @@
             this.$el.html(this.template({ subbreddits: this.collection }));
             return this;
         }
-    })
+    });
+
+    var PostsListView = Backbone.View.extend({
+        el: '<ul></ul>',
+        template: _.template('\
+            <% posts.each(function(post) { %>\
+                <li>\
+                    <a href="#"><%= post.get("title") %></a>\
+                    <% if (post.get("subbreddit")) { %>\
+                        <small><%= post.get("subbreddit").get("name") %></small>\
+                    <% } %>\
+                </li>\
+            <% }) %>\
+        '),
+
+        initialize: function() {
+            this.listenTo(this.collection, 'update', this.render);
+        },
+
+        render: function() {
+            this.$el.html(this.template({ posts: this.collection }));
+            return this;
+        }
+    });
 
 
     var homeView = new HomeView();
     $('#content').html(homeView.render().el);
-
-    
 
     </script>
 
