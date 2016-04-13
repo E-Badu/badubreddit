3c0b9a295f9cf37291acbbf63988531350ba0e94
Erica Badu <erica.a.badu@gmail.com>
2016-04-12 19:19:23 -0500

    fixed mispelling

--- gulpfile.js
+++ gulpfile.js
@@ -34,5 +34,5 @@ gulp.task('watch', function() {
 	gulp.watch('public/src/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('build', ['bundle','saas']);
+gulp.task('build', ['bundle','sass']);
 gulp.task('default', ['build', 'watch']);
